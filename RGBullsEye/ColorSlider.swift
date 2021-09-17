90bd558c92381d85347796207b8a265e82fc5ed3
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-09-17 10:58:32 +0200

    Game Skeleton

---
+++ RGBullsEye/ColorSlider.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSlider.swift
+//  RGBullsEye
+//
+//  Created by Amr Hesham on 17/09/2021.
+//
+
+import SwiftUI
+
+struct ColorSlider: View {
+    @Binding var value: Double
+   var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }
+    }
+}
--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double = Double.random(in: 0..<1)
+    @State var gGuess: Double = Double.random(in: 0..<1)
+    @State var bGuess: Double = Double.random(in: 0..<1)
+    
+    // MARK: - Body
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                        .padding()
+                    
+                }
+                
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))"
+                     + "  G: \(Int(gGuess * 255.0))"
+                     + "  B: \(Int(bGuess * 255.0))")
+                        .padding()
+                    
+                }
+
+            }
+            Button(action: {}) {
+                Text("Hit Me!")
+            }
+            
+            // <SliderView>
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+            // <SliderView/>
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
