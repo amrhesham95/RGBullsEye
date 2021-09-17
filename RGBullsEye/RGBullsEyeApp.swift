7eb800018f6b69867718f31630ff5e1c167dc130
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-09-17 10:16:54 +0200

    Initial commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Amr Hesham on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
